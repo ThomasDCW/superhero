9fc2ac9569fe28ec828d85e097c4bfadd048f5fd
Thomas_DCL_USR <thomas.dicola@user-agency.com>
2023-02-07 18:06:13 +0100

    theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, GlobalStyles, lightTheme } from './styles/theme';
+
 function App() {
-  return <div className='App'></div>;
+  const [theme, setTheme] = useState('light');
+  const isDarkTheme = theme === 'dark';
+  const toggleTheme = () => setTheme(isDarkTheme ? 'light' : 'dark');
+  return (
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <div className='App'>
+        <GlobalStyles />
+        <button onClick={toggleTheme}>
+          {isDarkTheme ? (
+            <span aria-label='Light mode' role='img'>
+              🌞
+            </span>
+          ) : (
+            <span aria-label='Dark mode' role='img'>
+              🌜
+            </span>
+          )}
+        </button>
+        <p>COUCOU</p>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Reset } from 'styled-reset';
 import App from './App';
+import Layout from './styles/Layout';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Layout>
+    <Reset />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Layout>
 );
